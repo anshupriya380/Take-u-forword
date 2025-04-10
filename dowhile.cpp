a1a0d5b7c4b357c3086d567292aae997f3a115ba
anshupriya380 <145607444+anshupriya380@users.noreply.github.com>
2025-04-11 00:07:18 +0530

    upload

---
+++ dowhile.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+// do while exceute atleast onece as time ,irrespective of the condition
+int main(){
+    int i=2;
+    do{
+        cout<< "Anshu "<<i << endl;
+        i++;
+    }while(i<=1);
+    
+    return 0;
+}
